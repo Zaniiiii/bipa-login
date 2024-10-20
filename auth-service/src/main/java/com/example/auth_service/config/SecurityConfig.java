cba82c4ef395f342f7dc15385b4e55ae008bc0ce
Hamdan Azani <hamdanpesat@gmail.com>
2024-10-21 05:22:26 +0700

    Feat: config cors

--- auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.*;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
