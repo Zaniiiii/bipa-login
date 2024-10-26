2a68aa08ef1f05567ab7ce3fd2765318d38006b8
Hamdan Azani <hamdanpesat@gmail.com>
2024-10-26 19:14:36 +0700

    Fix: allow all cors

--- user-service/user-service/src/main/java/com/example/auth_service/config/WebConfig.java
+++ user-service/user-service/src/main/java/com/example/auth_service/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Ganti dengan origin frontend Anda
+                        .allowedOrigins("*") // Ganti dengan origin frontend Anda
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
